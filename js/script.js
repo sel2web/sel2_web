59cde2b5f7405c1456928b48cb5fefe5ccc7b552
Junnosuke Hara <haraJnosuke@gmail.com>
2014-12-22 02:37:27 +0900

    修正

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
 var  fade = 0;
 
+
+
 $(function(){
 
-if(fade == 0){
-  $("#logo").animate({
-    'opacity':1,
-  },{
-    'duration': 2000,
-    'complete': function(){
-      fade = 2;
-    }
-  });
-}
+  if(fade == 0){
+    $("#logo").animate({
+      'opacity':1,
+    },{
+      'duration': 2000,
+      'complete': function(){
+        fade = 1;
+      }
+    });
+  }
+
 
 
   $(window).scroll(function(){
     var s = ($(this).scrollTop());
     var blur_v = (s-1100)/100  ;
-    var logo_alpha = (600 - s)/300
-    var back_alpha = -(570 -s)/300
+    var logo_a = (600 - s)/300;
+    var back_a = (s-550)/100;
+
+
 
 
 
@@ -33,23 +38,43 @@ if(fade == 0){
     }
 
     var vague = $(".container").Vague({intensity:blur_v});
+    var vague_2 = $(".container_2").Vague({intensity:blur_v});
+    var vague_3 = $(".container_3").Vague({intensity:blur_v});
     vague.blur();
+    vague_2.blur();
+    vague_3.blur();
 
-    $("#data").text('s = '+ s + 'px / blur_v ='+ blur_v + '/ logo_alpha = '+logo_alpha);
 
-      if(fade == 2){
+    $("#data").text('s = '+ s + 'px / blur_v ='+ blur_v + '/ logo_a = '+logo_a+'/ back_a = '+ back_a);
+
+      if(fade == 1){
         $("#logo").css(
-          "opacity", logo_alpha
+          "opacity", logo_a
         );
 
-        $("#back").css(
-          "opacity", back_alpha
-        );
+        if(s > 600){
+          $('.container').fadeIn(1200);
+          $('.container_2').fadeIn(1200);
+          $('.container_3').fadeIn(1200);
+
+        }else{
+          $('.container').fadeOut(100);
+          $('.container_2').fadeOut(100);
+          $('.container_3').fadeOut(100);
+
+
+        }
       }
 
+      $("#back").css(
+        "opacity", back_a
+      );
 
 
   });
+
+  $("#data_2").text(back_fade);
+
 
 
 
