aaf887235a411e02556bdf15cb0c3c020e158230
Junnosuke Hara <haraJnosuke@gmail.com>
2014-12-27 20:00:49 +0900

    12/26

--- js/script.js
+++ js/script.js
@@ -54,8 +54,8 @@ $(function(){
           $('.container_2').fadeIn(1200);
           $('.container_3').fadeIn(1200);
             if(menudown == false){
-              $('#menu_logo').animate({'top':20},{'duration':1000}),menudown = true;
-              $('#menu').animate({'top':20},{'duration':1000}),menudown = true;
+              $('.menu_logo').animate({'top':0},{'duration':1000}),menudown = true;
+              $('#menu').animate({'top':0},{'duration':1000}),menudown = true;
             }
         }else{
           $('.container').fadeOut(100);
@@ -65,7 +65,7 @@ $(function(){
 
         if(s < 600){
           if(menudown == true){
-            $('#menu_logo').animate({'top':-500},{'duration':500}),menudown = false;
+            $('.menu_logo').animate({'top':-500},{'duration':500}),menudown = false;
             $('#menu').animate({'top':-500},{'duration':500}),menudown = false;
 
           }
@@ -78,6 +78,37 @@ $(function(){
 
 
   });
+
+  $("#movie").hover(function(){
+      $("#movie").css("color","white").css("text-shadow"," 1px 1px 0px #000000","-1px 1px 0px #000000","1px -1px 0px #000000","-1px -1px 0px #000000")
+  },function(){
+    $("#movie").css("color","#4D4E4F").css("text-shadow","none")
+  });
+
+  $("#abour").hover(function(){
+    $("#about").css("color","#191919")
+  },function(){
+    $("#about").css("color","#4D4E4F")
+  });
+
+  $("#artists").hover(function(){
+    $("#artists").css("color","#191919")
+  },function(){
+    $("#artists").css("color","#4D4E4F")
+  });
+
+  $("#events").hover(function(){
+    $("#events").css("color","#191919")
+  },function(){
+    $("#events").css("color","#4D4E4F")
+  });
+
+  $("#acsess").hover(function(){
+    $("#acsess").css("color","#191919")
+  },function(){
+    $("#acsess").css("color","#4D4E4F")
+  });
+
 
 
 });
