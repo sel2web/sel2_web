ad28d3276be4563b51d31acbdf43b5a0af5f412b
Junnosuke Hara <haraJnosuke@gmail.com>
2015-01-03 15:11:14 +0900

    1/3

--- js/script.js
+++ js/script.js
@@ -35,9 +35,16 @@ $(function(){
     var vague = $(".container").Vague({intensity:blur_v});
     var vague_2 = $(".container_2").Vague({intensity:blur_v});
     var vague_3 = $(".container_3").Vague({intensity:blur_v});
+    var vague_4 = $(".container_4").Vague({intensity:blur_v});
+    var vague_5 = $(".container_5").Vague({intensity:blur_v});
+
+
     vague.blur();
     vague_2.blur();
     vague_3.blur();
+    vague_4.blur();
+    vague_5.blur();
+
 
 
     $("#data").text('s = '+ s + 'px / blur_v ='+ blur_v + '/ logo_a = '+logo_a+'/ back_a = '+ back_a);
@@ -54,20 +61,32 @@ $(function(){
           $('.container').fadeIn(1200);
           $('.container_2').fadeIn(1200);
           $('.container_3').fadeIn(1200);
+          $('.container_4').fadeIn(1200);
+        //  $('.container_5').fadeIn(1200);
+
+
             if(menudown == false){
               $('.menu_logo').animate({'top':0},{'duration':1000}),menudown = true;
               $('#menu').animate({'top':0},{'duration':1000}),menudown = true;
+              $('#twitter').animate({'bottom':20},{'duration':1000}),menudown = true;
+
             }
         }else{
           $('.container').fadeOut(100);
           $('.container_2').fadeOut(100);
           $('.container_3').fadeOut(100);
+          $('.container_4').fadeOut(100);
+          $('.container_5').fadeOut(100);
+
+
         }
 
         if(s < 600){
           if(menudown == true){
             $('.menu_logo').animate({'top':-500},{'duration':500}),menudown = false;
             $('#menu').animate({'top':-500},{'duration':500}),menudown = false;
+            $('#twitter').animate({'bottom':-200},{'duration':500}),menudown = false;
+
 
           }
         }
